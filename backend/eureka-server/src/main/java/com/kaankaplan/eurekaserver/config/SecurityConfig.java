fd5b7b535c943855075d9f1b227061297500671d
Wadgua <wadguakillian@gmail.com>
2026-03-05 13:51:22 +0100

    corrections made

--- backend/eureka-server/src/main/java/com/kaankaplan/eurekaserver/config/SecurityConfig.java
+++ backend/eureka-server/src/main/java/com/kaankaplan/eurekaserver/config/SecurityConfig.java
@@ -1,43 +1,43 @@
 package com.kaankaplan.eurekaserver.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+// @Configuration
+// @EnableWebSecurity
+public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder()) // BcryptPassword
-                .withUser("eureka")
-                .password(passwordEncoder().encode("password"))
-                .authorities("USER");
-    }
+    // @Override
+    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //     auth.inMemoryAuthentication()
+    //             .passwordEncoder(passwordEncoder()) // BcryptPassword
+    //             .withUser("eureka")
+    //             .password(passwordEncoder().encode("password"))
+    //             .authorities("USER");
+    // }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/actuator/health").permitAll()
-            .antMatchers("/actuator/info").permitAll()
-            .antMatchers("/actuator/**").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic();
-    }
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http
+    //         .csrf().disable()
+    //         .authorizeRequests()
+    //         .antMatchers("/actuator/health").permitAll()
+    //         .antMatchers("/actuator/info").permitAll()
+    //         .antMatchers("/actuator/**").permitAll()
+    //         .anyRequest().authenticated()
+    //         .and()
+    //         .httpBasic();
+    // }
 
 }
